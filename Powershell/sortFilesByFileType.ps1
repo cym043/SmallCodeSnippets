dee3237af03c828b8ea5fc4a4746534b8d01502c
cym043 <54802928+cym043@users.noreply.github.com>
2021-03-17 17:24:30 +0100

    Add files via upload

---
+++ Powershell/sortFilesByFileType.ps1
@@ -0,0 +1,16 @@
+﻿function sortFilesByExtension($path){
+    $arr = (dir $path)
+
+    for ($i=0;$i -lt $arr.Length; $i++){
+        $entry = $arr[$i].Extension
+        Write-Host $entry
+        if(Test-Path($path +"\"+ $arr[$i].Extension)){
+            Move-Item $arr[$i].FullName -Destination $arr[$i].Extension
+        }else{
+            New-Item -Path $path -Name $arr[$i].Extension -ItemType directory
+            Move-Item $arr[$i].FullName -Destination $arr[$i].Extension
+        }
+
+    }
+
+}
