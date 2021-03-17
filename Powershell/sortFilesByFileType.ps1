faaf15c3fd9b448f022a837fe574747434df541c
cym043 <54802928+cym043@users.noreply.github.com>
2021-03-17 17:39:52 +0100

    Update sortFilesByFileType.ps1

--- Powershell/sortFilesByFileType.ps1
+++ Powershell/sortFilesByFileType.ps1
@@ -1,11 +1,22 @@
 ﻿function sortFilesByExtension($path){
+    
+    #Get the files from the passed path and put them into an array
     $arr = (dir $path)
-
-    for ($i=0;$i -lt $arr.Length; $i++){
+    
+    #Iterate through the files by index
+    for($i=0;$i -lt $arr.Length; $i++){
+        
+        #Get the extension name
         $entry = $arr[$i].Extension
+        
+        #Write the file to the console
         Write-Host $entry
+        
+        #If the directory for file extension exists, move the file in the folder
         if(Test-Path($path +"\"+ $arr[$i].Extension)){
             Move-Item $arr[$i].FullName -Destination $arr[$i].Extension
+            
+        #Else create the folder and move the file into the created folder
         }else{
             New-Item -Path $path -Name $arr[$i].Extension -ItemType directory
             Move-Item $arr[$i].FullName -Destination $arr[$i].Extension
